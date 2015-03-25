5cbbfceaf0be7b9c87798cf4364da5c9b7335796
nicolas silva <sovietfrance@gmail.com>
2015-03-25 14:38:04 -0300

    initial commit

---
+++ src/app/components/main.jsx
@@ -0,0 +1,30 @@
+/** In this file, we create a React component which incorporates 
+    components provided by material-ui */
+
+var React = require('react'),
+  mui = require('material-ui'),
+  RaisedButton = mui.RaisedButton;
+
+var Main = React.createClass({
+
+  render: function() {
+
+    return (
+      <div className="example-page">
+
+        <h1>material-ui</h1>
+        <h2>example project</h2>
+
+        <RaisedButton label="Clave super secreta!" primary={true} onTouchTap={this._handleTouchTap} />
+
+      </div>
+    );
+  },
+
+  _handleTouchTap: function() {
+    alert('1-2-3-4-5');
+  }
+  
+});
+
+module.exports = Main;
